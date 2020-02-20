3a0925561f6e1499a2bdc600d260e6fe173dfa98
CD Santiago <slashlos@gmail.com>
2020-02-20 12:09:01 -0500

    segue away from data based file I/O

--- AppSandboxQuickStart/Document.swift
+++ AppSandboxQuickStart/Document.swift
@@ -59,12 +59,25 @@ class Document: NSDocument {
 		// Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
 		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
 	}
+	override func write(to url: URL, ofType typeName: String) throws {
+		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	}
 
 	override func read(from data: Data, ofType typeName: String) throws {
 		// Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
 		// Alternatively, you could remove this method and override read(from:ofType:) instead.
 		// If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
 		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	}
+	
+	override func read(from url: URL, ofType typeName: String) throws {
+		switch typeName {
+		case "AnyType":
+			fileURL = url
+			break
+		default:
+			throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		}
 	}
 
 	convenience init(contentsOf url: URL, ofType typeName: String) throws {
